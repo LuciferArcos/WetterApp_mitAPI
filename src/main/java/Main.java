dd7684c77ed83570f834f114c6d29b978b842e17
LuciferArcos <lucyluc2004@gmail.com>
2026-08-24 01:19:28 +0200

    war's erstmal für heute

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,11 @@
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.json.JSONObject;
 
@@ -13,12 +16,17 @@ public class Main {
         //               Wetterdaten werden lesbar angezeigt,
         //               Fehler werden abgefangen.
 
+        Scanner scanner = new Scanner(System.in);
+
         Dotenv dotenv = Dotenv.load();
         String apiKey = dotenv.get("API_KEY");
 
+        System.out.print("Von welcher Stadt möchtest du das Wetter wissen?: ");
+        String stadt = scanner.nextLine();
+        String encoded = URLEncoder.encode(stadt, StandardCharsets.UTF_8);
 
 
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=" + apiKey;
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + stadt + "&appid=" + apiKey;
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -30,6 +38,12 @@ public class Main {
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+            JSONObject obj = new JSONObject(response);
+
+            //String base = obj.getString("weather");
+            //int temp = obj.getInt();
+
+
             System.out.println(response.statusCode());
             System.out.println(response.body());
         }
@@ -39,6 +53,8 @@ public class Main {
         catch(IOException e){
             System.out.println("Fehler");
         }
+
+        scanner.close();
 
     }
 }
